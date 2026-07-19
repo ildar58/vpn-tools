f90e32cf51b3dcde434f2f50d23a00268d22a0a9
Ildar Dautov <48159586+ildar58@users.noreply.github.com>
2026-07-19 16:01:46 +0300

    Update vps-hardening-and-geo.sh

--- vps-hardening-and-geo.sh
+++ vps-hardening-and-geo.sh
@@ -75,8 +75,8 @@ TMPDIR="$(mktemp -d)"
 mkdir -p "$DIR"
 
 GEOSITE_URL="https://github.com/v2ray/domain-list-community/releases/latest/download/dlc.dat"
-GEOSITE_URL_2="https://github.com/1andrevich/Re-filter-lists/releaseslatest/download/geosite.dat"
-GEOIP_URL_2="https://github.com/1andrevich/Re-filter-lists/releaseslatest/download/geoip.dat"
+GEOSITE_URL_2="https://github.com/1andrevich/Re-filter-lists/releases/latest/download/geosite.dat"
+GEOIP_URL_2="https://github.com/1andrevich/Re-filter-lists/releases/latest/download/geoip.dat"
 
 curl -fsSL "$GEOSITE_URL" -o "$TMPDIR/geosite.dat"
 curl -fsSL "$GEOSITE_URL_2" -o "$TMPDIR/refilter.dat"
